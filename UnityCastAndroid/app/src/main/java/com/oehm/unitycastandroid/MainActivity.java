244052c43019523ce08e593c67f027d32ae4a859
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2015-08-14 00:12:45 +0200

    tested some stuff;

--- UnityCastAndroid/app/src/main/java/com/oehm/unitycastandroid/MainActivity.java
+++ UnityCastAndroid/app/src/main/java/com/oehm/unitycastandroid/MainActivity.java
@@ -1,19 +1,47 @@
 package com.oehm.unitycastandroid;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
 
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
 import com.unity3d.player.UnityPlayerNativeActivity;
 
 
 public class MainActivity extends UnityPlayerNativeActivity {
 
+    private MediaRouter mMediaRouter;
+    private MediaRouteSelector mMediaRouteSelector;
+    private MediaRouter.Callback mMediaRouterCallback;
+    private CastDevice mSelectedDevice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+
+        // Configure Cast device discovery
+        mMediaRouter = MediaRouter.getInstance(getApplicationContext());
+        mMediaRouteSelector = new MediaRouteSelector.Builder()
+                .addControlCategory(CastMediaControlIntent.categoryForCast(getResources()
+                        .getString(R.string.app_id))).build();//should be the required categories
+        mMediaRouterCallback = new MyMediaRouterCallback();
+
+    }
+
+    private class MyMediaRouterCallback extends MediaRouter.Callback {
+
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo info) {
+            mSelectedDevice = CastDevice.getFromBundle(info.getExtras());
+
+            //launchReceiver();
+        }
+
+        @Override
+        public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo info) {
+            //teardown(false);
+            mSelectedDevice = null;
+        }
     }
 }
