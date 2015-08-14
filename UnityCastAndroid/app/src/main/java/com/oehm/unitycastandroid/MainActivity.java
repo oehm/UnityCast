d0c11142168492da4f6cf238eff6ddf186262081
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2015-08-14 15:53:32 +0200

    useless shit...

--- UnityCastAndroid/app/src/main/java/com/oehm/unitycastandroid/MainActivity.java
+++ UnityCastAndroid/app/src/main/java/com/oehm/unitycastandroid/MainActivity.java
@@ -1,50 +1,36 @@
 package com.oehm.unitycastandroid;
 
 import android.os.Bundle;
-import android.support.v7.media.MediaRouteSelector;
-import android.support.v7.media.MediaRouter;
 import android.widget.Toast;
 
-import com.google.android.gms.cast.CastDevice;
-import com.google.android.gms.cast.CastMediaControlIntent;
+import com.google.android.libraries.cast.companionlibrary.cast.VideoCastManager;
 import com.unity3d.player.UnityPlayerNativeActivity;
 
 
 public class MainActivity extends UnityPlayerNativeActivity {
 
-    private MediaRouter mMediaRouter;
-    private MediaRouteSelector mMediaRouteSelector;
-    private MediaRouter.Callback mMediaRouterCallback;
-    private CastDevice mSelectedDevice;
+    private VideoCastManager mCastManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Toast.makeText(this, "v2", Toast.LENGTH_LONG).show();
 
-        // Configure Cast device discovery
-        mMediaRouter = MediaRouter.getInstance(getApplicationContext());
-        mMediaRouteSelector = new MediaRouteSelector.Builder()
-                .addControlCategory(CastMediaControlIntent.categoryForCast(getResources()
-                        .getString(R.string.app_id))).build();//should be the required categories
-        mMediaRouterCallback = new MyMediaRouterCallback();
+        VideoCastManager.checkGooglePlayServices(this);
 
-    }
+        VideoCastManager.
+                initialize(this, "794B7BBF", null, null)
+                .enableFeatures(
+                        VideoCastManager.FEATURE_LOCKSCREEN |
+                        VideoCastManager.FEATURE_DEBUGGING);
 
-    private class MyMediaRouterCallback extends MediaRouter.Callback {
 
-        @Override
-        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo info) {
-            mSelectedDevice = CastDevice.getFromBundle(info.getExtras());
+        mCastManager = VideoCastManager.getInstance();
 
-            //launchReceiver();
-        }
+        Toast.makeText(this, "v3", Toast.LENGTH_LONG).show();
 
-        @Override
-        public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo info) {
-            //teardown(false);
-            mSelectedDevice = null;
-        }
+
+        mCastManager.startCastDiscovery();
     }
 }
