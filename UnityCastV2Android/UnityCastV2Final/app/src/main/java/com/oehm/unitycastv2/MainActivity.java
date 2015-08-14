45e56b3c7e5a17466e177ec248bd1003aed38426
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2015-08-14 20:11:46 +0200

    v2 added; version with reversed tactic;

---
+++ UnityCastV2Android/UnityCastV2Final/app/src/main/java/com/oehm/unitycastv2/MainActivity.java
@@ -0,0 +1,98 @@
+package com.oehm.unitycastv2;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.media.MediaRouter;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.cast.ApplicationMetadata;
+import com.google.android.libraries.cast.companionlibrary.cast.VideoCastManager;
+import com.google.android.libraries.cast.companionlibrary.cast.callbacks.VideoCastConsumer;
+import com.google.android.libraries.cast.companionlibrary.cast.callbacks.VideoCastConsumerImpl;
+import com.google.android.libraries.cast.companionlibrary.cast.player.VideoCastControllerActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private VideoCastManager mCastManager;
+    private MenuItem mediaRouteMenuItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // initialize VideoCastManager
+        VideoCastManager.
+                initialize(this, "4F8B3483", VideoCastControllerActivity.class, null).
+                setVolumeStep(0.05).
+                enableFeatures(VideoCastManager.FEATURE_NOTIFICATION |
+                        VideoCastManager.FEATURE_LOCKSCREEN |
+                        VideoCastManager.FEATURE_WIFI_RECONNECT |
+                        VideoCastManager.FEATURE_CAPTIONS_PREFERENCE |
+                        VideoCastManager.FEATURE_DEBUGGING);
+
+        VideoCastManager.checkGooglePlayServices(this);
+        setContentView(R.layout.activity_main);
+
+        mCastManager = VideoCastManager.getInstance();
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        mediaRouteMenuItem = mCastManager.
+                addMediaRouterButton(menu, R.id.media_route_menu_item);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume() was called");
+        mCastManager = VideoCastManager.getInstance();
+        if (null != mCastManager) {
+            mCastManager.incrementUiCounter();
+        }
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mCastManager.decrementUiCounter();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy is called");
+        super.onDestroy();
+    }
+}
