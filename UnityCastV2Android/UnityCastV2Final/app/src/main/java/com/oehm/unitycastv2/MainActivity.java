caee9d0fc4cc17f3b5264dae4aa7e0a4acfb30bb
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2015-08-16 16:20:34 +0200

    fuck;

--- UnityCastV2Android/UnityCastV2Final/app/src/main/java/com/oehm/unitycastv2/MainActivity.java
+++ UnityCastV2Android/UnityCastV2Final/app/src/main/java/com/oehm/unitycastv2/MainActivity.java
@@ -28,26 +28,11 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private VideoCastManager mCastManager;
-    private MenuItem mediaRouteMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // initialize VideoCastManager
-        VideoCastManager.
-                initialize(this, "4F8B3483", VideoCastControllerActivity.class, null).
-                setVolumeStep(0.05).
-                enableFeatures(VideoCastManager.FEATURE_NOTIFICATION |
-                        VideoCastManager.FEATURE_LOCKSCREEN |
-                        VideoCastManager.FEATURE_WIFI_RECONNECT |
-                        VideoCastManager.FEATURE_CAPTIONS_PREFERENCE |
-                        VideoCastManager.FEATURE_DEBUGGING);
 
-        VideoCastManager.checkGooglePlayServices(this);
-        setContentView(R.layout.activity_main);
-
-        mCastManager = VideoCastManager.getInstance();
     }
 
 
@@ -56,9 +41,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateOptionsMenu(menu);
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        mediaRouteMenuItem = mCastManager.
-                addMediaRouterButton(menu, R.id.media_route_menu_item);
 
         return true;
     }
@@ -81,17 +63,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         Log.d(TAG, "onResume() was called");
-        mCastManager = VideoCastManager.getInstance();
-        if (null != mCastManager) {
-            mCastManager.incrementUiCounter();
-        }
 
         super.onResume();
     }
 
     @Override
     protected void onPause() {
-        mCastManager.decrementUiCounter();
         super.onPause();
     }
 
@@ -104,27 +81,7 @@ public class MainActivity extends AppCompatActivity {
     public void startUnity(View view) {
         // Do something in response to button
 
-
-        if(mCastManager.isConnected()){
-
-            MediaMetadata mediaMetadata = new MediaMetadata(MediaMetadata.MEDIA_TYPE_MOVIE);
-            mediaMetadata.putString(MediaMetadata.KEY_TITLE, "Demo Video");
-
-            try {
-                mCastManager.loadMedia(new MediaInfo.Builder(
-                        "http://video.webmfiles.org/big-buck-bunny_trailer.webm")
-                        .setContentType("video/webm")
-                        .setStreamType(MediaInfo.STREAM_TYPE_BUFFERED)
-                        .setMetadata(mediaMetadata)
-                        .build(),true,0);
-
-            } catch (TransientNetworkDisconnectionException | NoConnectionException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-        Intent intent = new Intent(this, CustomUnityPlayerActivity.class);
+        Intent intent = new Intent(this, UnityPlayerActivity.class);
         startActivity(intent);
     }
 }
