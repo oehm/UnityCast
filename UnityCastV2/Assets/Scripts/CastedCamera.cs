70c0c83ebdd74b1e599cbb046061a202a99a3d9f
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2015-08-18 01:57:55 +0200

    tests with cast cam;

---
+++ UnityCastV2/Assets/Scripts/CastedCamera.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(Camera))]
+public class CastedCamera : MonoBehaviour
+{
+
+	//public Material debugMaterial;
+
+	// Use this for initialization
+	void Start ( )
+	{
+		Camera cam = GetComponent<Camera>( );
+
+		//if ( Display.displays.Length != 2)
+		//{
+		//	Display tmpMainDisplay = Display.displays[0];
+
+
+		//	Display.displays = new Display[]
+		//	{
+		//		tmpMainDisplay,
+		//		new Display()
+		//	};
+
+		//	debugMaterial.color = Color.red;
+		//}
+
+		if ( Display.displays.Length > 1 )
+		{
+			Display castDisplay = Display.displays[1];
+
+			castDisplay.SetRenderingResolution( 512, 512 );
+			cam.SetTargetBuffers( castDisplay.colorBuffer, castDisplay.depthBuffer );
+		}
+		cam.enabled = Display.displays.Length > 1;
+	}
+
+}
