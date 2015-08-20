aa065c5babcfc67dca29a43c182c041e165e9ea2
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2015-08-20 20:01:44 +0200

    restructuring and project cleanup;

---
+++ UnityCastV2/Assets/Test Stuff/Scripts/Profiler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Profiler : MonoBehaviour
+{
+	public Text renderedFrameCountText;
+	public Text realtimeText;
+
+	private long _renderedFrameCount;
+
+	private void OnEnable ( )
+	{
+		_renderedFrameCount = 0;
+	}
+
+	private void Update ( )
+	{
+		_renderedFrameCount++;
+		if ( renderedFrameCountText != null)
+			renderedFrameCountText.text = "" + _renderedFrameCount;
+
+		if ( realtimeText != null )
+			realtimeText.text = "" + Time.realtimeSinceStartup;
+	}
+}
