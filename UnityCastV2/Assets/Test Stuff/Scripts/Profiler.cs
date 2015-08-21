a1943f3f6ce139e8a4808a0a274eaa26114f543b
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2015-08-21 14:42:15 +0200

    Improved profiler and profiler display a ton;

--- UnityCastV2/Assets/Test Stuff/Scripts/Profiler.cs
+++ UnityCastV2/Assets/Test Stuff/Scripts/Profiler.cs
@@ -4,23 +4,65 @@ using System.Collections;
 
 public class Profiler : MonoBehaviour
 {
-	public Text renderedFrameCountText;
-	public Text realtimeText;
+	public Text[] frameTexts;
+	public Text[] timeTexts;
+	public Image blinker;
 
-	private long _renderedFrameCount;
+	private int _textIndex;
+
+	private long _frameCount;
+
+	private bool _isSetUpCorrectly;
 
 	private void OnEnable ( )
 	{
-		_renderedFrameCount = 0;
+		_frameCount = 0;
+
+		_textIndex = 0;
+
+		_isSetUpCorrectly = frameTexts.Length >= 2 && timeTexts.Length == frameTexts.Length && blinker != null;
+
+		if ( !_isSetUpCorrectly )
+		{
+			enabled = false;
+		}
 	}
 
 	private void Update ( )
 	{
-		_renderedFrameCount++;
-		if ( renderedFrameCountText != null)
-			renderedFrameCountText.text = "" + _renderedFrameCount;
+		if ( !_isSetUpCorrectly )
+		{
+			return;
+		}
 
-		if ( realtimeText != null )
-			realtimeText.text = "" + Time.realtimeSinceStartup;
+		_frameCount++;
+
+		int lastTextIndex = _textIndex;
+		_textIndex = ( _textIndex + 1 ) % frameTexts.Length;
+		int nextTextIndex = ( _textIndex + 1 ) % frameTexts.Length;
+
+		
+		frameTexts[lastTextIndex].color = Color.black;
+
+		frameTexts[_textIndex].color = Color.red;
+		frameTexts[_textIndex].text = _frameCount.ToString( "000000" );
+		;
+
+		frameTexts[nextTextIndex].text = "";
+
+
+		timeTexts[lastTextIndex].color = Color.black;
+
+		timeTexts[_textIndex].color = Color.red;
+		timeTexts[_textIndex].text = Time.realtimeSinceStartup.ToString( "00.000" );
+		;
+
+		timeTexts[nextTextIndex].text = "";
+
+		blinker.enabled = (int) ( Time.realtimeSinceStartup ) % 2 == 0;
+
 	}
+
+
+
 }
